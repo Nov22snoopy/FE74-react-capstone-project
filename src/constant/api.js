0cee3e2955710a11cabd1aee84512cf6a3b67a1d
nov22snoopy <nguyenquangvu1988@gmail.com>
2023-05-01 09:41:13 +0700

    init folder

---
+++ src/constant/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const TokenCyberSoft =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udGVuZCA3NCIsIkhldEhhblN0cmluZyI6IjE2LzA5LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5NDgyMjQwMDAwMCIsIm5iZiI6MTY2ODI3MjQwMCwiZXhwIjoxNjk0OTcwMDAwfQ.3TXoqM7cOKUQgRGc0plbpUsV406snlZBBeHlA7RxJYk'
+const baseURL = 'https://movienew.cybersoft.edu.vn/api/'
+
+const http = axios.create()
+
+http.interceptors.request.use((config) => {
+    console.log(localStorage.getItem('user'));
+    return {
+        ...config,
+        headers: {
+            TokenCyberSoft,
+            //  Authorization: `Bearer ${JSON.parse(localStorage.getItem('user')?.accessToken || '')}`
+        },
+        baseURL,
+    }
+})
